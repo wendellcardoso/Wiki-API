1b28a70badd233fd73075e05eac1f28306c95bc7
Wendell Cardoso <cardosowendell@gmail.com>
2021-01-07 17:01:50 -0300

    DELTE All Articles

--- app.js
+++ app.js
@@ -45,4 +45,14 @@ app.post("/articles", (req, res) => {
     });
 });
 
+app.delete("/articles", (req, res) => {
+    Article.deleteMany((err) => {
+        if (!err) {
+            res.send("Successfully deleted all articles");
+        } else {
+            res.send(err);
+        }
+    })
+})
+
 app.listen(3000, () => { console.log("Server started on port 3000.") });
