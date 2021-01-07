d8210791fe3f413532a04193f08ece7869f7dc5f
Wendell Cardoso <cardosowendell@gmail.com>
2021-01-07 12:15:28 -0300

    POST a New Article

--- app.js
+++ app.js
@@ -29,4 +29,20 @@ app.get("/articles", (req, res) => {
     })
 });
 
+app.post("/articles", (req, res) => {
+
+    const newArticle = new Article({
+        title: req.body.title,
+        content: req.body.content
+    });
+
+    newArticle.save((err) => {
+        if (!err) {
+            res.send("Succesfully added a new article.");
+        } else {
+            res.send(err);
+        }
+    });
+});
+
 app.listen(3000, () => { console.log("Server started on port 3000.") });
