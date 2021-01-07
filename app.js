035e566ed4ac9d697da4c6d82b5d584d7d2009fb
Wendell Cardoso <cardosowendell@gmail.com>
2021-01-07 11:44:40 -0300

    GET all articles

--- app.js
+++ app.js
@@ -3,13 +3,14 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 
-mongoose.connect("mongodb://localhost:27017/wikiDB", { useNewUrlParser: true, useUnifiedTopology: true });
-
 const app = express();
 
 app.set("view engine", "ejs");
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/wikiDB", { useNewUrlParser: true, useUnifiedTopology: true });
 
 const articleSchema = new mongoose.Schema({
     title: String,
@@ -17,5 +18,15 @@ const articleSchema = new mongoose.Schema({
 });
 
 const Article = mongoose.model("Article", articleSchema);
+
+app.get("/articles", (req, res) => {
+    Article.find((err, foundArticles) => {
+        if (!err) {
+            res.send(foundArticles);
+        } else {
+            res.send(err);
+        }
+    })
+});
 
 app.listen(3000, () => { console.log("Server started on port 3000.") });
